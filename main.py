8e747f43ee829ad7dcc3aa25e8dfbab1aeed7396
Shiri Itach <shiritach@gmail.com>
2023-02-15 21:56:25 +0200

    heuristics

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+import timeit
+
 import aStar
 import idaStar
-import euclideanDistanceHeuristic
+import heuristics
 
 
 def main():
     start_state = [[1, 2, 3], [5, 6, 0], [7, 8, 4]]
     goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-    heuristic = euclideanDistanceHeuristic.heuristic
+    heuristic = heuristics.euclidean_distance_heuristic
+    # heuristic = heuristics.manhattan_distance_heuristic()
 
-    solution1 = aStar.a_star(start_state, goal_state,heuristic)
-    solution2 = idaStar.ida_star(start_state, goal_state,heuristic)
+    solution1 = aStar.a_star(start_state, goal_state, heuristic)
+    solution2 = idaStar.ida_star(start_state, goal_state, heuristic)
 
-    print("A* result: "+ str(solution1))
-    print("IDA*  result: "+ str(solution2))
+    print("A* result: " + str(solution1))
+    print("IDA*  result: " + str(solution2))
+
 
 if __name__ == "__main__":
     main()
